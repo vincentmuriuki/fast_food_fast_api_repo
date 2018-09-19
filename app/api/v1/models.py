c03bfdfe37173e9c6397bf084314eaa9bff3fc9f
Vincent <vincentmuriuki42@gmail.com>
2018-09-19 17:19:53 +0300

    Feature (Description) Add dictionary datastructure
    This dictionary stores clients' order-data
    
    Order requirements i.e. name of the food, the price, and description are specified
    
    [finishes #160611147]

---
+++ app/api/v1/models.py
@@ -0,0 +1,28 @@
+food_orders = []
+
+class Order:
+
+    order_id = 1
+    def __init__(self,name=None,price=None,description=None, status="Pending"):
+        self.name=name
+        self.price=price
+        self.description=description
+        self.id=Order.order_id
+        self.status=status
+        
+
+        Order.order_id += 1
+
+    def serialize(self):
+        return dict(
+            id=self.id,
+            name=self.name,
+            price=self.price,
+            description=self.description,
+            status=self.status
+        )
+
+    def retrieve_order_by_id(self, order_id):
+        for order in food_orders:
+            if order.id == order_id:
+                return order
