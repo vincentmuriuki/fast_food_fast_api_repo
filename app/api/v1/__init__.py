576097a9819bfc88faf33420b43e1333fa04ad81
VINCENT MURIUKI <vincentmuriuki42@gmail.com>
2018-09-25 16:02:45 +0300

    Update __init__.py

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -5,7 +5,7 @@ from flask_restful import Api
 
 # local imports
 from instance.config import app_config
-from views import SingleOrder, PostOrder, GetOrders
+from .views import SingleOrder, PostOrder, GetOrders
 
 
 def create_app(config_stage):
