d0092ad318ca7ec67410ff567ab51bd89a6b1c15
VINCENT MURIUKI <vincentmuriuki42@gmail.com>
2018-09-25 17:06:10 +0300

    Update __init__.py

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -5,7 +5,7 @@ from flask_restful import Api
 
 # local imports
 from instance.config import app_config
-from .views import SingleOrder, PostOrder, GetOrders
+from .views import PostOrder, GetOrders
 
 
 def create_app(config_stage):
@@ -14,7 +14,7 @@ def create_app(config_stage):
 
     api = Api(app)
 
-    api.add_resource(SingleOrder, '/api/v1/orders/<int:id>')
+#     api.add_resource(SingleOrder, '/api/v1/orders/<int:id>')
     api.add_resource(PostOrder, '/api/v1/orders')
     api.add_resource(GetOrders, '/api/v1/orders')
 
