2b5487db2f4ea3a033191515239992f25d0b701e
VINCENT MURIUKI <vincentmuriuki42@gmail.com>
2018-09-25 17:32:02 +0300

    Update views.py

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -14,29 +14,29 @@ class PostOrder(Resource):
         food_orders.append(order)
         return {"message":"Congratulations. Your new order has been posted. Kindly wait!"}, 201
 
-class GetOrders(Resource):
-    def get(self):
-        return {"Orders":[order.serialize() for order in food_orders]}
+# class GetOrders(Resource):
+#     def get(self):
+#         return {"Orders":[order.serialize() for order in food_orders]}
 
-class SingleOrder(Resource):
-    def get(self, id):
-        spec_order = CustomerOrders().retrieve_order_by_id(id)
+# class SingleOrder(Resource):
+#     def get(self, id):
+#         spec_order = CustomerOrders().retrieve_order_by_id(id)
         
-        if spec_order:
-            return {"order": spec_order.serialize()}, 200
-        return {"message":"Order not found"}, 404
+#         if spec_order:
+#             return {"order": spec_order.serialize()}, 200
+#         return {"message":"Order not found"}, 404
 
-    def put(self, id):
-        order = CustomerOrders().retrieve_order_by_id(id)
-        if order:
-            order.status="approved"
-            return {"message":"status approved"}
-        return {"message":"Order not found"}, 404
+#     def put(self, id):
+#         order = CustomerOrders().retrieve_order_by_id(id)
+#         if order:
+#             order.status="approved"
+#             return {"message":"status approved"}
+#         return {"message":"Order not found"}, 404
 
 
-    def delete(self, id):
-        spec_order = CustomerOrders().retrieve_order_by_id(id)
-        if spec_order:
-            food_orders.remove(spec_order)
-            return {"Message" : "Requested Order deleted successfully!"},200
-        return {"message" : "Requested Order not found! Try a different ID!"}, 404
+#     def delete(self, id):
+#         spec_order = CustomerOrders().retrieve_order_by_id(id)
+#         if spec_order:
+#             food_orders.remove(spec_order)
+#             return {"Message" : "Requested Order deleted successfully!"},200
+#         return {"message" : "Requested Order not found! Try a different ID!"}, 404
