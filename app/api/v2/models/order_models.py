d2480ae69dd4ac944ae0528a7ac58110625b4d5e
Vincent <vincentmuriuki42@gmail.com>
2018-09-28 10:57:48 +0300

    [Feature #160843819] This adds order models This file contains order definitions for posting an order It describes the parameters needed to post an order that is name, price, and description

--- app/api/v2/models/order_models.py
+++ app/api/v2/models/order_models.py
@@ -1,52 +1,45 @@
-from app.api.v2 import db
-from flask_bcrypt import Bcrypt
-from flask import current_app
-import jwt
-from datetime import datetime, timedelta
+from datetime import datetime
 
+# from app.api.v2.database.fastfood_tables import dbconn
+from app.api.v2.database.tables import dbconn
 
-class Orders(db.Model):
-	# table columns
-	__tablename__ = 'customer_orders'
+class Orders:
+    table = 'orders'
 
-	order_id = db.Column(db.Integer, primary_key=True)
-	ordered_by = db.Column(db.Integer, db.ForeignKey(User.user_id))
-	order_date = db.Column(db.DateTime, default=db.func.current_timestamp())
-	date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-		onupdate=db.func.current_timestamp())
-	price = db.Column(db.String(500))
-	name = db.Column(db.String(500))
-	status = db.Column(db.Boolean)
+    def __init__(self, name, quantity, price):
+        self.name = name
+        self.quantity = quantity
+        self.price = price
+        self.date_created = datetime.now()
+        self.status = 'Pending'
 
-	def __init__(self, name, price, status, ordered_by):
-		self.name = name
-		self.price = price
-		self.status = status
-		self.ordered_by = ordered_by
+    def __repr__(self):
+        return f'<Order {self.name}'
 
-	def save(self):
-		"""
-		This function saves the order details
-		"""
-		db.session.add(self)
-		db.session.commit()
+    def add_to_list(self):
+        return {
+            'name': self.name,
+            'quantity': self.quantity,
+            'price': self.price,
+            'date_created': self.date_created,
+            'status': self.status
+        }
 
-	@staticmethod
-	def get_all_user_orders(user_id):
-		"""
-		Function to retrieve all orders by a specific user
-		"""
-		return Order.query.filter_by(ordered_by=user_id)
+    def add_order(self):
+        order = Orders(self.name,
+                       self.quantity,
+                       self.price)
+        pass
 
-	def delete(self):
-		"""
-		This function deletes a specific order given order_id
-		"""
-		db.session.delete(self)
-		db.session.commit()
+    
+    @classmethod
+    def find_by_id(cls, order_id):
+        pass
 
-	def __repr__(self):
-		"""
-		This function returns a representation of an order
-		"""
-		return "<Order: {}>".format(self.name)
+    @staticmethod
+    def delete(order_id):
+        pass
+
+    @staticmethod
+    def delete_all():
+        pass
