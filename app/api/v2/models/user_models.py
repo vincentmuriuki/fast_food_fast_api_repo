b2afd6468a1bbfc627862fb1523359b29558321e
Vincent <vincentmuriuki42@gmail.com>
2018-10-02 16:12:20 +0300

    [Chore #160922674] This model describes the parameters used when registering a new user; i.e username, email, location, and password, and role It also creates the connection to the database, commits the changes and retrieves the user information from the database

--- app/api/v2/models/user_models.py
+++ app/api/v2/models/user_models.py
@@ -24,5 +24,23 @@ class User(object):
             return self.email
 
 
-    		
+    def user_creation(self, username, email, location, password, role = False):        
+        self.username = username
+        self.email = email
+        self.location = location
+        self.password = password
+        self.role = role
+
+        cursor = self.connection.cursor()
+        cursor.execute("""INSERT INTO users (username, email, password, location, role) 
+        VALUES ('%s', '%s', '%s', '%s', '%s') RETURNING customer_id
+        """ % (self.username, self.email, self.password, self.location, self.user))
+        customer_id = cursor.fetchone()
+        self.connection.commit()
+        cursor.execute("SELECT * FROM users WHERE email='%s'" % self.email)
+
+        customer = cursor.fetchone()
+        print("Welcome" + str(customer))
+
+        return customer		
 	
