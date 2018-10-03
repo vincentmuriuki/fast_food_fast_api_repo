cfdc659c12ce11c9747a8e4bbfd97c50ce6e990e
Vincent <vincentmuriuki42@gmail.com>
2018-10-03 14:25:53 +0300

    [Bug #160953191] This fixes the get all orders endpoint

--- app/api/v2/models/order_models.py
+++ app/api/v2/models/order_models.py
@@ -13,13 +13,12 @@ from flask import jsonify
 
 
 class OrderModels(object):
-    """ This class will hold all methods for user authentication """
-	url = os.environ.get('URL')
-	connection = psycopg2.connect(url)
-	@staticmethod        
-	def retrieve_all_orders():
-		cursor = OrderModels.connection.cursor()
-		cursor.execute("SELECT * FROM orders")
-		rows = cursor.fetchall()
-		cursor.close()
-		return rows
+		url = os.environ.get('URL')
+		connection = psycopg2.connect(url)
+		@staticmethod        
+		def retrieve_all_orders():
+			cursor = OrderModels.connection.cursor()
+			cursor.execute("SELECT * FROM orders")
+			rows = cursor.fetchall()
+			cursor.close()
+			return rows
