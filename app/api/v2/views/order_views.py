e0e851457de0a35d5047fde05c4d1bf9ef77b06d
Vincent <vincentmuriuki42@gmail.com>
2018-10-03 13:54:00 +0300

    [Feature #160952583] This adds the  orders endpoint When this request is run, all orders should be displayed This url is only accessible by the admin user

---
+++ app/api/v2/views/order_views.py
@@ -0,0 +1,38 @@
+
+from flask import Flask, request, jsonify, make_response
+from flask_restful import reqparse, Resource, Api
+
+
+from app.api.v2.models.order_models import OrderModels
+
+
+class PostOrder(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True, help="Food name is required!")
+        parser.add_argument('description', type=str, required=True, help="Food description is required!")
+        parser.add_argument('price', type=str, required=True, help="Price is required!")
+
+        args = parser.parse_args()
+        order_data = request.get_json()
+        name = order_data['name']
+        description = order_data['description']
+        price = order_data['price']
+        order_models = OrderModels(name=name, description=description, price=price)
+        order_id = order_models.order_creation()
+        return(
+            {
+                "message":"Order created successfully",
+                "order_id":order_id[0]
+            }
+        ), 201
+
+class RetrieveAllOrders(Resource):
+    def get(self):
+        """
+        Thus function retrieves all orders from the database
+        Only Admin has access to this route
+        """
+        order = OrderModels.retrieve_all_orders()
+        print("print",order)
+        return ({"Orders" : order}), 200
